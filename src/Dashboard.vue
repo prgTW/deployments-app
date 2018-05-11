6215cf2fbf84a189502f17839d431e31f1317353
Tomasz Wójcik <tomasz.prgtw.wojcik@gmail.com>
2018-05-11 21:05:43 +0200

    looks

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -52,7 +52,7 @@
 					</v-list-tile-content>
 
 					<v-list-tile-action>
-						<v-btn color="blue" flat small round slot="activator" :href="commit.raw.url"
+						<v-btn :icon="$vuetify.breakpoint.smAndDown" color="blue" outline small round slot="activator" :href="commit.raw.url"
 							   target="_blank">
 							<v-icon class="hidden-md-and-up">link</v-icon>
 							<strong class="hidden-sm-and-down">

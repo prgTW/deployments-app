37e13da73bb985b37c3ce2162ac113e5040e6ac7
Tomasz Wójcik <tomasz.prgtw.wojcik@gmail.com>
2018-05-11 23:10:48 +0200

    star icon

--- src/Sidebar.vue
+++ src/Sidebar.vue
@@ -13,7 +13,7 @@
 			</v-list-tile-content>
 			<v-list-tile-action>
 				<v-btn icon ripple>
-					<v-icon color="grey lighten-1">info</v-icon>
+					<v-icon color="grey lighten-1">star_border</v-icon>
 				</v-btn>
 			</v-list-tile-action>
 		</v-list-tile>
